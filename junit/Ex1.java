4675757b69b67a5fc2b4886cf49a192aa9ce9af1
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2020-10-31 12:17:47 +0900

    jUnitの練習

---
+++ junit/Ex1.java
@@ -0,0 +1,77 @@
+package junit;
+
+class Ex1{
+	/**
+	 *
+	 * @param n
+	 * @return
+	 * 	n > 0 => "その値は正です"
+	 * 	n <= 0 => "その値は０か負です"
+	 */
+	public String ex1_0(int n){
+
+		return "dummy";
+
+	}
+
+	public int ex1_1(int n){
+
+		return -1;
+
+	}
+
+	public String ex1_2(int a, int b){
+
+		return "dummy";
+	}
+
+	public String ex1_3(int a, int b){
+
+		return "dummy";
+
+	}
+
+	public String ex1_4(int a){
+
+		return "dummy";
+
+	}
+
+	public String ex1_5(int a){
+
+		return "dummy";
+	}
+
+	public String ex1_6(int a){
+		return "dummy";
+	}
+
+	public String ex1_7(int a){
+		return "dummy";
+	}
+
+	public double ex1_8(double a, double b){
+		return -1.0;
+	}
+
+	public int ex1_9(int a, int b){
+		return 0;
+	}
+
+	public String ex1_10(int a, int b){
+		return "dummy";
+	}
+
+	public int ex1_11(int a, int b, int c){
+		return 1000;
+	}
+
+	public int ex1_12(int a, int b, int c){
+		return 1000;
+	}
+
+	public String ex1_13(int a){
+		return "dummy";
+	}
+}
+
