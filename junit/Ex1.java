fa0eb20fc79b7d622d8f9f9e2757daa5ee0d3b3d
Aniya Keita <s20001@std.it-college.ac.jp>
2020-11-18 03:01:46 +0900

    Ex1.java

--- junit/Ex1.java
+++ junit/Ex1.java
@@ -13,7 +13,7 @@ class Ex1{
 			return "その値は正です";
 		}
 		else {
-			return "dummy";
+			return "その値は0か負です";
 		}
 
 	}
@@ -31,56 +31,100 @@ class Ex1{
 
 	public String ex1_2(int a, int b){
 		if (a % b == 0)
-				return( a + "は" + b + "の約数です");
+				return( b + "は" + a + "の約数です");
 		else
-			return (a + "は" + b + "の約数ではありません");
+			return (b + "は" + a + "の約数ではありません");
 		}
 
 	public String ex1_3(int a, int b){
-		return
+		if (a > b) 
+			return a + "の方が大きいです";
+		else if (a < b)
+			return b + "の方が大きいです";
+		else
+			return "同じ値です";
 	}
 
 	public String ex1_4(int a){
-
-		return "dummy";
+		if (a < 0) 
+			return "正でない整数値です";
+		else if(a % 5 == 0) 
+			return "その値は5で割り切れます";
+		else 
+			return "その値は5で割り切れません";
 
 	}
 
 	public String ex1_5(int a){
-
-		return "dummy";
+		if (a < 0) 
+			return "正でない整数値です";
+		else if (a % 10 == 0) 
+			return "その値は10の倍数です";
+		else 
+			return "その値は10の倍数ではありません";
 	}
 
 	public String ex1_6(int a){
-		return "dummy";
+		if (a % 3 == 0) 
+			return "その値は3で割り切れます";
+		else if (a % 3 == 1) 
+			return "その値を3で割った余りは1です";
+		else
+			return "その値を3で割った余りは2です";
 	}
 
-	public String ex1_7(int a){
-		return "dummy";
+	public String ex1_7(int a) {
+		if (a < 0 || a > 100) 
+			return "範囲(0～100)外の値です";
+		else if (a <= 59)
+			return
 	}
 
 	public double ex1_8(double a, double b){
-		return -1.0;
+		return a > b ? a : b;
 	}
 
 	public int ex1_9(int a, int b){
-		return 0;
+		int sa = a- b;
+		
 	}
 
 	public String ex1_10(int a, int b){
-		return "dummy";
+		int sa = a - b;
+		if (sa < 0)
+			sa *= -1;
+		
+		if 
 	}
 
 	public int ex1_11(int a, int b, int c){
+		if (a < b)
+			if (a < c)
+				return a;
+			else
+				return c;
+		else if (b < c)
+			return 
 		return 1000;
 	}
 
 	public int ex1_12(int a, int b, int c){
-		return 1000;
+		if (a < b);
+		if (a < c)
+			if (b < c)
+				return b;
+			else
+				return c;
+		else
+			if (a < c)
+				return a;
+			else
+				return c;
 	}
 
 	public String ex1_13(int a){
-		return "dummy";
+		String [] retStr =;
+		["冬","冬","春","春","春","夏","夏","夏",""]
 	}
 }
 
