0fd168cde474f8e3e0cf1b598f8f88dcd2a00c16
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-11-18 17:47:15 +0900

    SL: Lesson 11/16

--- junit/Ex2.java
+++ junit/Ex2.java
@@ -1,66 +1,88 @@
 package junit;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Ex2{
 
 	public int [] ex2_1(int [] a){
 
-		return a;
-
+		Arrays.sort(a); return a;
 	}
 
 	public int [] ex2_2(int [] a){
 
+		Arrays.sort(a);
+		for (int i = 0; i < a.length/2; i++) {
+			int temp = a[i];
+			a[i] = a[a.length - i -1];
+			a[a.length -i -1] = temp;
+		}
 		return a;
-
 	}
 
-	public String [] ex2_3(int [] a){
+	public ArrayList<String> ex2_3(int [] a){
 
-		String [] hantei = {"dummy"};
+//		ArrayList<String> l = new ArrayList<>();
+		String [] l = new String[a.length];
 
-		return hantei;
-
+		for(int i=0; i<a.length; i++) {
+			l[i] = a[i] > 0 ? "正" : a[i] < 0 ? "負" : "零";
+		}
+        return l;
 	}
 
 	public int ex2_4(int a){
 
-		return -1;
-
+		int x = 0;
+		while (a > 0) {
+			x++; a /= 10;
+		}
+		return x;
 	}
 
 	public int ex2_5(int a){
 
-		return -1;
+		int x =  0;
+		for (int i = 0; i <= a; i++) {
+			x += i;
+		}
+		return x;
 
 	}
 
 	public int [] ex2_6(int a){
 
-		int [] ret = new int[1];
-
-		return ret;
-
+//		TEST
 	}
 
 	public boolean ex2_7(int a){
 
-		return false;
-
+		boolean x = (a == 1) ? false : true;
+//				: (a == 2) ? true
+//				: (a % 2 == 0) ? false : true;
+		for (int i = 2; i < a; i++) {
+			if (a % 2 == 0) {
+				x = true;
+			} else {
+				x = false;
+			}
+		}
+		return x;
 	}
 
 	public int ex2_8(int [] a){
 
-		int ret = 0;
-
-		return ret;
-
+		int x = IntStream.of(a).sum();
+		return x;
 	}
 
 	public int ex2_9(int [] a){
 
-		int ret = 0;
-
-		return ret;
+		double x = IntStream.of(a).sum();
+		return (int) Math.round(x / Array.getLength(a));
 
 	}
 }
