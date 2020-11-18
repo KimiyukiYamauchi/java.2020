56fc7ae926cc464592dc7f3cd29f6119f8581f3f
s20024 <s20024@std.it-college.ac.jp>
2020-11-18 12:48:25 +0900

    Ex2.javaの実装

--- junit/Ex2.java
+++ junit/Ex2.java
@@ -1,67 +1,94 @@
 package junit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Ex2{
 
 	public int [] ex2_1(int [] a){
-
+		Arrays.sort(a);
 		return a;
 
 	}
 
 	public int [] ex2_2(int [] a){
-
-		return a;
+		Arrays.sort(a);
+		int[] pas = new int[a.length];
+		for (int i = 0; i < a.length ; i ++) {
+			pas[i] = a[a.length - i - 1];
+		}
+		return pas;
 
 	}
 
 	public String [] ex2_3(int [] a){
-
-		String [] hantei = {"dummy"};
-
+		String [] hantei = new String[a.length];
+		for (int i = 0; i < a.length; i ++) {
+			if (a[i] < 0) {
+				hantei[i] = "負";
+			} else if (a[i] > 0) {
+				hantei[i] = "正";
+			} else {
+				hantei[i] = "零";
+			}
+		}
 		return hantei;
 
 	}
 
 	public int ex2_4(int a){
-
-		return -1;
-
+		String s = String.valueOf(a);
+		return s.length();
 	}
 
 	public int ex2_5(int a){
-
-		return -1;
-
+		int count = 0;
+		for (int i = 1; i <= a; i++) {
+			count = count + i;
+		}
+		return count;
 	}
 
 	public int [] ex2_6(int a){
-
-		int [] ret = new int[1];
-
+		ArrayList<Integer> passing = new ArrayList<Integer>();
+		for (int i = 1;i <= a; i ++) {
+			if (a % i == 0) {
+				passing.add(i);
+			}
+		}
+		int [] ret = new int[passing.size()];
+		for (int i = 0; i < ret.length; i ++) {
+			ret[i] = passing.get(i).intValue();
+		}
 		return ret;
 
 	}
 
 	public boolean ex2_7(int a){
-
-		return false;
-
+		int n = 0;
+		for (int i = 1; i <= a; i ++) {
+			if (a % i == 0) {
+				n ++;
+			}
+		}
+		return (n == 2);
 	}
 
 	public int ex2_8(int [] a){
-
 		int ret = 0;
-
+		for (int i :a) {
+			ret = ret + i;
+		}
 		return ret;
 
 	}
 
 	public int ex2_9(int [] a){
-
-		int ret = 0;
-
-		return ret;
-
+		double ret = 0;
+		for (int i: a) {
+			ret = ret + i;
+		}
+		return (int) Math.round(ret / a.length);
 	}
 }
 
