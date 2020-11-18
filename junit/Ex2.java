2b66179977d855c9915ea894309074ba2d52903e
s20014 <s20014@std.it-college.ac.jp>
2020-11-18 13:58:31 +0900

    Ex2

--- junit/Ex2.java
+++ junit/Ex2.java
@@ -1,67 +1,120 @@
 package junit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Ex2{
 
 	public int [] ex2_1(int [] a){
 
+		Arrays.sort(a);
 		return a;
+
 
 	}
 
-	public int [] ex2_2(int [] a){
-
-		return a;
+	public Integer[] ex2_2(Integer[] test){
+		Arrays.sort(test, Collections.reverseOrder());
+		return test;
 
 	}
 
-	public String [] ex2_3(int [] a){
+	public String[] ex2_3(int [] a){
+		String[] mozi = {"a","a","a","a","a","a","a"};
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > 0) {
+				mozi[i] = "正";
+			}else if(a[i] < 0) {
+				mozi[i] = "負";
+			}else {
+				mozi[i] = "零";
+			}
+		}
 
-		String [] hantei = {"dummy"};
+		return mozi;
 
-		return hantei;
+
+
+
 
 	}
 
 	public int ex2_4(int a){
 
-		return -1;
+		return String.valueOf(a).length();
 
 	}
 
 	public int ex2_5(int a){
-
-		return -1;
+		int num = 0;
+		for(int i = 0; i <= a; i ++) {
+			num += i;
+		}
+		return num;
 
 	}
 
-	public int [] ex2_6(int a){
+	public int[] ex2_6(int a){
+		List<Integer> ap = new ArrayList<Integer>();
+		for (int i = 1; i <= a; i++) {
+			if (a % i == 0) {
+				ap.add(i);
+			}
+		}
 
-		int [] ret = new int[1];
+			int [] ret = new int[ap.size()];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = ap.get(i).intValue();
+			}
 
 		return ret;
 
 	}
 
 	public boolean ex2_7(int a){
+		int num = 0;
 
-		return false;
+		for (int i = 1; i <= a; i ++) {
+			if (a % i == 0){
+				num++;
+			}
 
+		}
+		return (num == 2);
 	}
+
+
 
 	public int ex2_8(int [] a){
 
-		int ret = 0;
+		int sum = 0;
 
-		return ret;
+		for(int i = 0; i < a.length; i ++) {
+			sum += a[i];
+		}
+
+
+		return sum;
 
 	}
 
 	public int ex2_9(int [] a){
 
-		int ret = 0;
+		double sum = 0;
+		double num = 0;
 
-		return ret;
+		for(int i = 0; i < a.length; i ++) {
+			sum += a[i];
+
+		 num = sum / a.length;
+
+
 
 	}
+		return (int)Math.round(num);
 }
+}
+
 
