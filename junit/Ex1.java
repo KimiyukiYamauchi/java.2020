0d51c5696dd044ce92d58017b9062510d0f7d645
ryota kuniyoshi <s20007@std.it-college.ac.jp>
2020-11-18 12:10:52 +0900

    Ex完了

--- junit/Ex1.java
+++ junit/Ex1.java
@@ -6,72 +6,212 @@ class Ex1{
 	 * @param n
 	 * @return
 	 * 	n > 0 => "その値は正です"
-	 * 	n <= 0 => "その値は０か負です"
+	 * 	n <= 0 => "その値はか負です"
 	 */
 	public String ex1_0(int n){
-
-		return "dummy";
+		
+		if (n > 0 ) {
+			return "その値は正です";
+			
+		}else {
+			return "その値は０か負です";
+		}
 
 	}
 
 	public int ex1_1(int n){
+		if (n > 0) {
+			return 1;
 
-		return -1;
+		}else if (n < 0) {
+			return 1;
+
+		}else {
+			return 0;
+		}
+
 
 	}
 
 	public String ex1_2(int a, int b){
+		
+		if (a % b == 0) {
+			return b + "は" + a + "の約数です";
 
-		return "dummy";
+		}else {
+			return b + "は" + a + "の約数ではありません";
+		}
 	}
 
 	public String ex1_3(int a, int b){
+		
+		if (a > b) {
+			return a +"の方が大きいです";
 
-		return "dummy";
+		}else if (a < b) {
+			return b +"の方が大きいです";
 
+		}else {
+			return "同じ値です";
+		}
 	}
 
 	public String ex1_4(int a){
-
-		return "dummy";
-
+		
+		if (a < 0) {
+			return "正でない整数値です";
+			
+		}else if (a % 5 == 0) {
+			return "その値は5で割り切れます";
+			
+		}else {	
+			return "その値は5で割り切れません";
+		}
 	}
 
 	public String ex1_5(int a){
-
-		return "dummy";
+		
+		if (a < 0) {
+			return "正でない整数値です";
+			
+		}else if (a % 10 == 0) {
+			return "その値は10の倍数です";
+			
+		}else {
+			return "その値は10の倍数ではありません";
+		}
 	}
 
 	public String ex1_6(int a){
-		return "dummy";
+		
+		if (a < 0) {
+			return "正でない整数値です";
+
+		}else if (a % 3 == 0) {
+			return "その値は3で割り切れます";
+
+		}else if (a % 3 == 1) {
+			return "その値を3で割った余りは1です";
+
+		}else {
+			return "その値を3で割った余りは2です";
+		}
 	}
+		
 
 	public String ex1_7(int a){
-		return "dummy";
+		
+		if (a < 0 || a > 100) {
+			return "範囲(0～100)外の値です";
+
+		}else if (a >= 0 && a <= 59) {
+			return "不可";
+
+		}else if (a >= 60 && a <= 69) {
+			return "可";
+
+		}else if (a >= 70 && a <= 79) {
+			return "良";
+
+		}else {
+			return "優";
+		}
 	}
 
 	public double ex1_8(double a, double b){
-		return -1.0;
+
+		if (a == b) {
+			return a;
+
+		}else if (a > b) {
+			return a;
+
+		}else {
+			return b;
+		}
 	}
 
 	public int ex1_9(int a, int b){
-		return 0;
+
+		return (a - b);
+
 	}
 
 	public String ex1_10(int a, int b){
-		return "dummy";
+
+		if (a - b == 10 || a - b == -10) {
+			return "それらの差は10以下です";
+
+		}else {
+			return "それらの差は11以上です";
+		}
 	}
 
 	public int ex1_11(int a, int b, int c){
-		return 1000;
+		
+		if (a >= b) {
+			if (b >= c) {
+				return c;
+
+			}else if (c >= a) {
+				return b;
+
+			}else {
+				return b;
+
+			}
+		}else {
+			if (c <= a) {
+				return c;
+
+			}else if (b <= c) {
+				return a;
+
+			}else {
+				return a;
+			}
+		}
 	}
 
 	public int ex1_12(int a, int b, int c){
-		return 1000;
+		if (a > b) {
+			if (b > c) {
+				return b;
+			}else if (c > a) {
+				return a;
+			}else {
+				return c;
+			}
+		}else {
+			if (c < a) {
+				return a;
+			}else if (b < c) {
+				return b;
+			}else {
+				return c;
+			}
+		}
 	}
 
 	public String ex1_13(int a){
-		return "dummy";
+		if (a >= 3 && a <= 5) {
+			return "春";
+
+		}else if (a >= 6 && a <= 8) {
+			return "夏";
+
+		}else if (a >= 9 && a <= 11) {
+			return "秋";
+
+		}else if (a <= 0) {
+			return "範囲(1～12)外の値です";
+
+		}else if (a > 12) {
+			return "範囲(1～12)外の値です";
+
+		}else {
+			return "冬";
+		}
 	}
 }
 
