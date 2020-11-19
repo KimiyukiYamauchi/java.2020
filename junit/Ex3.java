909807276d381e5f153c351758f50e751890c5b7
s20008 <s20008@std.it-college.ac.jp>
2020-11-19 12:21:07 +0900

    Ex3

--- junit/Ex3.java
+++ junit/Ex3.java
@@ -3,9 +3,14 @@ package junit;
 class Ex3{
 
 	public double ex3_1(int [] a){
-
-		double ret = 0;
-
-		return ret;
+		
+		double sum = 0;
+	    
+		for(int i = 0;i < a.length; i ++) {
+			sum += a[i];
+		}
+		double avg = sum / a.length;
+		return avg;
 	}
 }
+	
