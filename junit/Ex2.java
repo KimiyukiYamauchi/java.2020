0a94f890bd40a07412e961fb392b62543608f05d
Shuta Uehara <s20003@std.it-college.ac.jp>
2020-11-19 13:44:32 +0900

    Ex2.java実装

--- junit/Ex2.java
+++ junit/Ex2.java
@@ -1,8 +1,14 @@
 package junit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 class Ex2{
 
 	public int [] ex2_1(int [] a){
+
+		Arrays.sort(a);
 
 		return a;
 
@@ -10,13 +16,29 @@ class Ex2{
 
 	public int [] ex2_2(int [] a){
 
+		Integer [] b = new Integer[a.length];
+
+		for (int i = 0; i < a.length; i++) {
+			b[i] = a[i];
+		}
+
+		Arrays.sort(b, Collections.reverseOrder());
+
+		for (int i = 0 ; i < b.length; i++) {
+			a[i] = b[i];
+		}
+
 		return a;
 
 	}
 
 	public String [] ex2_3(int [] a){
 
-		String [] hantei = {"dummy"};
+		String [] hantei = new String[a.length];
+
+		for (int i = 0; i < a.length; i++) {
+			hantei[i] = a[i] > 0 ? "正" : a[i] < 0 ? "負" : "零";
+		}
 
 		return hantei;
 
@@ -24,27 +46,60 @@ class Ex2{
 
 	public int ex2_4(int a){
 
-		return -1;
+		String str = a + "";
+
+		return str.length();
 
 	}
 
 	public int ex2_5(int a){
 
-		return -1;
+		int sum = 0;
+
+		for (int i = 1; i <= a; i++) {
+			sum += i;
+		}
+
+		return sum;
 
 	}
 
 	public int [] ex2_6(int a){
 
-		int [] ret = new int[1];
+		ArrayList<Integer> yakusu = new ArrayList<Integer>();
+
+		yakusu.add(1);
+
+		for (int i = 2; i <= a; i++) {
+			if (a % i == 0) {
+				yakusu.add(i);
+			}
+		}
+
+		int [] ret = new int[yakusu.size()];
+
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = yakusu.get(i);
+		}
 
 		return ret;
-
 	}
 
 	public boolean ex2_7(int a){
 
-		return false;
+		int cnt = 0;
+
+		if (a <= 1) {
+			return false;
+		}
+
+		for (int i = 2; i < a/2; i++) {
+			if (a % i == 0) {
+				cnt++;
+			}
+		}
+
+		return cnt == 0 ? true : false;
 
 	}
 
@@ -52,13 +107,25 @@ class Ex2{
 
 		int ret = 0;
 
+		for (int i : a) {
+			ret += i;
+		}
+
 		return ret;
 
 	}
 
 	public int ex2_9(int [] a){
 
-		int ret = 0;
+		double sum = 0;
+
+
+		for (int i : a) {
+			sum += i;
+		}
+
+		double avg = sum / a.length;
+		int ret = (int)Math.round(avg);
 
 		return ret;
 
