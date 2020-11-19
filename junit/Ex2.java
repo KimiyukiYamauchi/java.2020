5131df3774cc8b60b14899029b87d82cf5d61b2b
Gackt syukumine <s20012@std.it-college.ac.jp>
2020-11-19 10:28:49 +0900

    課題

--- junit/Ex2.java
+++ junit/Ex2.java
@@ -1,56 +1,98 @@
 package junit;
 
-class Ex2{
+import java.util.ArrayList;
+import java.util.Arrays;
 
-	public int [] ex2_1(int [] a){
+class Ex2 {
 
+	public int [] ex2_1(int [] a) {
+		Arrays.sort(a);
 		return a;
 
 	}
 
 	public int [] ex2_2(int [] a){
-
+		 for(int i = 0; i < a.length-1; i++){
+			 for(int j = i+1; j < a.length; j++){
+				 if(a[j] > a[i]){
+					 int temp = a[i];
+					 a[i] = a[j];
+					 a[j] = temp;
+	             }
+	         }
+	    }
 		return a;
 
 	}
 
 	public String [] ex2_3(int [] a){
 
-		String [] hantei = {"dummy"};
-
+		String[] hantei = new String[7];
+		for(int i = 0; i < a.length; i++) {
+			if(a[i] == 0) {
+				hantei[i] = "零";
+			}else if(a[i] > 0) {
+				hantei[i] = "正";
+			}else if(a[i] < 0) {
+				hantei[i] = "負";
+			}
+		}
 		return hantei;
 
 	}
 
 	public int ex2_4(int a){
-
-		return -1;
+		a = String.valueOf(a).length(); 
+		return a;
 
 	}
 
 	public int ex2_5(int a){
-
-		return -1;
+		int ret = 0;
+		for(int i = 0; i <= a; i++) {
+			ret = ret + i;
+		}
+		return ret;
 
 	}
 
 	public int [] ex2_6(int a){
 
-		int [] ret = new int[1];
-
+		int num[] = new int[a];
+		ArrayList<Integer> hoge = new ArrayList<Integer>();
+	    for(int i=1;i<= a;i++){
+	    	if(a % i == 0){
+	    		hoge.add(i);
+	        }
+	    }
+	    int [] ret = new int[hoge.size()];
+	    for (int s = 0; s < ret.length;s++){
+	        ret[s] = hoge.get(s);
+	    }
+	    
 		return ret;
 
 	}
 
 	public boolean ex2_7(int a){
-
+		for(int i = 2; i < a; i++) {
+            if(a % i == 0) {
+                return false;
+            }
+        }
+		if(a == 1) {
 		return false;
+		}
+		return true;
 
 	}
 
 	public int ex2_8(int [] a){
 
 		int ret = 0;
+		for(int i = 0; i <= a.length-1; i++) {
+			ret = ret + a[i];
+		}
 
 		return ret;
 
@@ -59,8 +101,15 @@ class Ex2{
 	public int ex2_9(int [] a){
 
 		int ret = 0;
+		int sum = a.length;
+		for(int i = 0; i <= a.length-1; i++) {
+			ret = ret + a[i];
+		}
+		double c = (double)ret / sum;
+	    c = Math.round(c);
+	    int s = (int)c;
 
-		return ret;
+		return s;
 
 	}
 }
