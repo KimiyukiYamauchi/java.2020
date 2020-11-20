7ffa1ae57d219f9850c3b3f1858d1435ca5d10d2
Seiya Zayasu <s20011@std.it-college.ac.jp>
2020-11-20 17:35:50 +0900

    Ex2

--- junit/Ex2.java
+++ junit/Ex2.java
@@ -1,5 +1,8 @@
 package junit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Ex2{
 
 	public int [] ex2_1(int [] a){
@@ -9,6 +12,14 @@ class Ex2{
 	}
 
 	public int [] ex2_2(int [] a){
+		Integer [] b = new Integer[a.length];
+
+		Arrays.sort(a);
+		for(int i = 0; i < a.length/2; i++) {
+			int temp = a[i];
+			a[i] = a[a.length -i -1];
+			a[a.length -i -1] = temp;
+		}
 
 		return a;
 
@@ -16,7 +27,15 @@ class Ex2{
 
 	public String [] ex2_3(int [] a){
 
-		String [] hantei = {"dummy"};
+		String [] hantei = new String[a.length];
+		for(int i = 0; i < a.length; i++) {
+			if(a[i] > 0)
+				hantei[i] = "正";
+			if(a[i] == 0)
+				hantei[i] = "零";
+			else
+				hantei[i] = "負";
+		}
 
 		return hantei;
 
@@ -29,22 +48,43 @@ class Ex2{
 	}
 
 	public int ex2_5(int a){
+		int sum = 0;
+		for(int i = 0; i <= a ; i++) {
+			sum += i;
+		}
 
-		return -1;
+		return sum;
 
 	}
 
 	public int [] ex2_6(int a){
+		ArrayList<Integer> yakusuu = new ArrayList<Integer>();
+		yakusuu.add(1);
+		for(int i = 2; i <= a; i++) {
+			if(a % i == 0)
+				yakusuu.add(i);
+		}
 
-		int [] ret = new int[1];
+		int [] ret = new int[yakusuu.size()];
+		for(int i = 0; i < ret.length; i++) {
+			ret[i] = yakusuu.get(i);
+		}
 
 		return ret;
 
 	}
 
 	public boolean ex2_7(int a){
+		int cnt = 0;
 
-		return false;
+		if(a <= 1) {
+			return false;
+		}
+		for(int i = 0; i <= a / 2 ; i++) {
+			if(a % i == 0)
+				cnt++;
+		}
+		return cnt == 0 ? true : false;
 
 	}
 
@@ -52,13 +92,23 @@ class Ex2{
 
 		int ret = 0;
 
+		for(int i : a) {
+			ret += i;
+
+		}
+
 		return ret;
 
 	}
 
 	public int ex2_9(int [] a){
+		double sum = 0;
 
-		int ret = 0;
+		for(int i : a) {
+			sum += i;
+		}
+		sum /= a.length;
+		int ret = (int)Math.round(sum);
 
 		return ret;
 
