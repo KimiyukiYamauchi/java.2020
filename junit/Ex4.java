eaa50cbcdcc0eafa5c83ddb7fe0b0480e2ecc50f
Seiya Zayasu <s20011@std.it-college.ac.jp>
2020-11-22 23:12:27 +0900

    Ex4

--- junit/Ex4.java
+++ junit/Ex4.java
@@ -1,66 +1,130 @@
 package junit;
 
+import java.util.ArrayList;
+
 class Ex4{
 
     public int [] ex4_1(){
 
-        int [] ret = new int[1];
+        int [] ret = {5, 4, 3, 2, 1};
 
         return ret;
     }
 
     public double [] ex4_2(){
 
-        double [] ret = new double[1];
+        double [] ret = {1.1, 2.2, 3.3, 4.4, 5.5};
 
         return ret;
     }
 
     public int [] ex4_3(int a, int b){
 
-        int [] ret = new int[1];
+        int [] ret = new int[a];
+        for(int i = 0; i < a; i++) {
+        	ret[i] = b;
+        }
 
         return ret;
     }
 
     public int [] ex4_4(int [] a){
 
-        int [] ret = new int[1];
+    	double t = 0;
+    	for(int i = 0; i < a.length; i++) {
+    		t += a[i];
+    	}
+
+    	int max = a[0];
+    	int min =a[0];
+
+    	for(int i = 0; i < a.length; i++) {
+    		if(max < a[i])
+    			max = a[i];
+    		if(min > a[i])
+    			min = a[i];
+    	}
+
+        int [] ret = new int[4];
+        int ave = (int)Math.round(t / a.length);
+
+        ret[0] = (int)t;
+        ret[1] = ave;
+        ret[2] = max;
+        ret[3] = min;
 
         return ret;
     }
 
     public int ex4_5(int [] a, int key){
 
-        int ret = 9999;
+    	for(int i = 0; i < a.length; i++) {
+    		if(a[i] == key )
+    			return i;
+    	}
 
-        return ret;
+
+
+        return -1;
     }
 
     public int ex4_6(int [] a, int key){
 
-        int ret = 9999;
+        int ret = 0;
+        for(int i = a.length -1; i > 0; i--) {
+        	if(a[i] == key)
+        		return ret = i;
+        }
 
-        return ret;
+        return ret = -1;
     }
 
     public int [] ex4_7(int [] a, int idx){
+    	ArrayList<Integer> list = new ArrayList<Integer>();
+    	for(int i = 0; i < a.length; i++) {
+    		if(i != idx)
+    			list.add(a[i]);
+    	}
 
-        int [] ret = new int[1];
+        int [] ret = new int[list.size()];
+
+        for(int i = 0; i < ret.length; i++) {
+        	ret[i] = list.get(i);
+        }
 
         return ret;
     }
 
     public int [] ex4_8(int [] a, int idx, int n){
+    	ArrayList<Integer> list = new ArrayList<Integer>();
+    	ArrayList<Integer> list2 = new ArrayList<Integer>();
+    	for(int i = 0; i < a.length; i++) {
+    		if(i >= idx && i <= idx + n)
+    			list2.add(a[i]);
 
-        int [] ret = new int[1];
+    		list.add(a[i]);
+    	}
 
+    	int [] ret = new int[list.size()];
+    	for(int i = 0; i < ret.length; i++) {
+    		ret[i] = list.get(i);
+    	}
         return ret;
     }
 
     public int [] ex4_9(int [] a, int idx, int x){
+    	int c = 0;
+    	int [] ret = new int[a.length + 1];
+    	for(int i = 0; i < a.length; i++) {
+    		if(i == idx) {
+    			ret[c] = x;
+    			c++;
+    		}
+    		ret[c] = a[i];
+    		c++;
+    	}
 
-        int [] ret = new int[1];
+
 
         return ret;
     }
