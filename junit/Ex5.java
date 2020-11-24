50be0e0b8b5acbc02e71a7c8c749060e5a870b45
s20024 <s20024@std.it-college.ac.jp>
2020-11-24 13:01:49 +0900

    Ex5.javaの実装

--- junit/Ex5.java
+++ junit/Ex5.java
@@ -3,65 +3,98 @@ package junit;
 class Ex5{
 
 	public int ex5_1(int a, int b){
-
-		return 0;
+		if (a < b) {
+			return a;
+		}
+		return b;
 	}
 
 	public int ex5_1(int a, int b, int c){
-
-		return 0;
+		int min = a;
+		int[] t = new int[]{a, b, c};
+		for (int i : t) {
+			if (i < min) {
+				min = i;
+			}
+		}
+		return min;
 	}
 
 	public int ex5_1(int [] a){
-
-		return 0;
+		int min = a[0];
+		for (int i : a) {
+			if (i < min) {
+				min = i;
+			}
+		}
+		return min;
 	}
 
 	public int ex5_2(int x){
-
+		if ( x < 0) {
+			return 0 - x;
+		}
 		return x;
 	}
 
 	public long ex5_2(long x){
-
+		if (x < 0) {
+			return 0 - x;
+		}
 		return x;
 	}
 
 	public float ex5_2(float x){
-
+		if (x < 0) {
+			return 0 - x;
+		}
 		return x;
 	}
 
 	public double ex5_2(double x){
-
+		if (x < 0) {
+			return 0 - x;
+		}
 		return x;
 	}
 
+
 	public String ex5_3(byte x){
-
-		String ret = "";
-
+		String ret = String.format("%8s", Integer.toBinaryString(x & 0xFF)).replace(' ', '0');
 		return ret;
 	}
 
 	public String ex5_3(short x){
-
-		String ret = "";
-
+		String ret = String.format("%16s", Integer.toBinaryString(x & 0xffff)).replace(' ', '0');
 		return ret;
 	}
 
 	public String ex5_3(int x){
-
-		String ret = "";
-
+		String ret = String.format("%32s", Integer.toBinaryString(x & 0xffffffff)).replace(' ', '0');
 		return ret;
 	}
 
 	public String ex5_3(long x){
-
+		boolean judge = (x < 0);
 		String ret = "";
-
+		if (judge) {
+			x = x * -1 - 1;
+		}
+		int[] passing = new int[64];
+		for ( int i = 63; i >= 0 ; i --) {
+			passing[i] = (int) (x % 2);
+			x = x / 2;
+		}
+		for ( int i: passing) {
+			if (judge) {
+				if (i == 0) {
+					i = 1;
+				} else {
+					i = 0;
+				}
+			}
+			ret = ret + i;
+		}
 		return ret;
 	}
 }
